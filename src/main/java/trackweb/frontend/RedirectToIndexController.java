2824b000113f2cdd4eb4c3185463711793ef0975
ben <ben@ben-MS-7982>
2018-07-12 23:20:28 +0200

    homepage redirect.

--- src/main/java/trackweb/frontend/RedirectToIndexController.java
+++ src/main/java/trackweb/frontend/RedirectToIndexController.java
@@ -20,15 +20,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Redirect to index.jsf SPA.
- * @author Marcelo Fernandes
- */
 @Controller
 public class RedirectToIndexController  {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String redirect() {
-		return "redirect:pm:TripView";
+		return "tripview.xhtml";
 	}
 }
